a8eff34776473a6c88409dab462ad5a1e036f55c
Bhawantha Athapattu <99388728+IT21253508@users.noreply.github.com>
2022-05-18 11:09:56 +0530

    Update Group_main.cpp

--- Group_main.cpp
+++ Group_main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <cstring>
 
+//adding headers
 #include "RegisteredUser.h"
 #include "FeedbackandReview.h"
 #include "BusStaff.h"
@@ -12,11 +13,14 @@
 #include "Route_Category.h"
 #include "Ticket.h"
 #include "BusCategory.h"
+#include "booking.h"
+#include "cncl.h"
 
 using namespace std;
 
 int main()
 {
+    //creating objects
     RegisteredUser u1, u2, u3, u4, u5;
     FeedbackandReview f1, f2, f3, f4, f5;
     BusStaff bs1, bs2, bs3, bs4, bs5;
@@ -26,9 +30,14 @@ int main()
     Route_Category r1, r2, r3, r4, r5;
     Ticket t1, t2, t3, t4, t5;
     BusCategory bc1, bc2, bc3, bc4, bc5;
+    Booking N1, N2, N3, N4, N4;
+    Cancellation CA1, CA2, CA3, CA4;
+    
 
     return 0;
 }
+
+//method calling
 RegisteredUser* user = new user(); 
 FeedbackandReview* feed = new feed(); 
 BusStaff* staff = new staff(); 
@@ -38,3 +47,20 @@ Payment* payment = new payment();
 Route_Category* route = new route(); 
 Ticket* ticket = new ticket(); 
 BusCategory* bcat = new bcat();
+Booking* book = new book();
+Cancellation* cancel = new cancel();
+
+//delete objects
+delete RegisteredUse;
+delete FeedbackandReview;
+delete BusStaff;
+delete Card;
+delete Bus_info;
+delete Payment;
+delete Payment;
+delete Route_Category;
+delete Ticket;
+delete Ticket;
+delete BusCategory;
+delete Booking;
+delete Cancellation;
